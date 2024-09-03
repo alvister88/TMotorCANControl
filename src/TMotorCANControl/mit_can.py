2da3c3c7105113fc9457cda300b9f6cf54867be7
alvister88 <ayzhu2022@gmail.com>
2024-09-02 18:57:43 -0700

    Changes to mit_can params

--- src/TMotorCANControl/mit_can.py
+++ src/TMotorCANControl/mit_can.py
@@ -31,14 +31,14 @@ MIT_Params = {
             'P_max' : 10.0,
             'V_min' : -100.0,
             'V_max' : 100.0,
-            'T_min' : -80.0,
-            'T_max' : 80.0,
+            'T_min' : -150.0,
+            'T_max' : 150.0,
             'Kp_min': 0.0,
             'Kp_max': 500.0,
             'Kd_min': 0.0,
             'Kd_max': 5.0,
             'Kt_TMotor' : 0.2, # from TMotor website (actually 1/Kvll)
-            'Current_Factor' : 1.0, # to correct the qaxis current 
+            'Current_Factor' : 0.59, # to correct the qaxis current 
             'Kt_actual': 0.2,# Need to use the right constant -- 0.115 by our calcs, 0.091 by theirs. At output leads to 1.31 by them and 1.42 by us.
             'GEAR_RATIO': 10.0, # hence the 9 in the name
             'Use_derived_torque_constants': False, # true if you have a better model
